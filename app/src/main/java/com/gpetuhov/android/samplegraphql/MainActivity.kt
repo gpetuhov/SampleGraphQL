b265577c0e0185946f29ef533e8bc8b0ba91bbf5
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-07 18:34:23 +0300

    Send Apollo query

--- app/src/main/java/com/gpetuhov/android/samplegraphql/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplegraphql/MainActivity.kt
@@ -2,7 +2,11 @@ package com.gpetuhov.android.samplegraphql
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.apollographql.apollo.ApolloCall
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.pawegio.kandroid.toast
 import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
@@ -17,5 +21,19 @@ class MainActivity : AppCompatActivity() {
             .serverUrl("https://graphql-pokemon.now.sh/graphql")
             .okHttpClient(okHttpClient)
             .build()
+
+        val query = PokemonNumberQuery.builder()
+            .name("Pikachu")
+            .build()
+
+        apolloClient.query(query).enqueue(object : ApolloCall.Callback<PokemonNumberQuery.Data>() {
+            override fun onFailure(e: ApolloException) {
+                runOnUiThread { toast("Error fetching Pikachu") }
+            }
+
+            override fun onResponse(response: Response<PokemonNumberQuery.Data>) {
+                runOnUiThread { toast("Pikachu number is: ${response.data()?.pokemon()?.number()}") }
+            }
+        })
     }
 }
