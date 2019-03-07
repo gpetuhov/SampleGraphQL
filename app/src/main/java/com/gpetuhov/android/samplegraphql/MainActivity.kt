8eb859a7e741d82ebed8d8892b8e5dbea5f7fa38
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-03-07 18:46:02 +0300

    Add load and clear buttons

--- app/src/main/java/com/gpetuhov/android/samplegraphql/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/samplegraphql/MainActivity.kt
@@ -7,33 +7,63 @@ import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.exception.ApolloException
 import com.pawegio.kandroid.toast
+import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val HOST = "https://graphql-pokemon.now.sh/graphql"
+        const val POKEMON = "Pikachu"
+    }
+
+    private lateinit var apolloClient: ApolloClient
+    private lateinit var pokemonNumberQuery: PokemonNumberQuery
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        initApolloClient()
+        initQuery()
+
+        queryButton.setOnClickListener { queryPokemonNumber() }
+        clearButton.setOnClickListener { clearText() }
+    }
+
+    private fun initApolloClient() {
         val okHttpClient = OkHttpClient.Builder().build()
 
-        val apolloClient = ApolloClient.builder()
-            .serverUrl("https://graphql-pokemon.now.sh/graphql")
+        apolloClient = ApolloClient.builder()
+            .serverUrl(HOST)
             .okHttpClient(okHttpClient)
             .build()
+    }
 
-        val query = PokemonNumberQuery.builder()
-            .name("Pikachu")
+    private fun initQuery() {
+        pokemonNumberQuery = PokemonNumberQuery.builder()
+            .name(POKEMON)
             .build()
+    }
 
-        apolloClient.query(query).enqueue(object : ApolloCall.Callback<PokemonNumberQuery.Data>() {
+    private fun queryPokemonNumber() {
+        apolloClient.query(pokemonNumberQuery).enqueue(object : ApolloCall.Callback<PokemonNumberQuery.Data>() {
             override fun onFailure(e: ApolloException) {
+                // Changing UI must be on UI thread
                 runOnUiThread { toast("Error fetching Pikachu") }
             }
 
             override fun onResponse(response: Response<PokemonNumberQuery.Data>) {
-                runOnUiThread { toast("Pikachu number is: ${response.data()?.pokemon()?.number()}") }
+                // Changing UI must be on UI thread
+                runOnUiThread {
+                    val text = "Pikachu number is: ${response.data()?.pokemon()?.number()}"
+                    textView.text = text
+                }
             }
         })
+    }
+
+    private fun clearText() {
+        textView.text = ""
     }
 }
